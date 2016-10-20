b28cea146c1a22b24f5d8446cbbd6ca8af9e2065
mike <viriyananda.michael@gmail.com>
2016-10-20 16:31:01 +0700

    Revisition

--- purchase_request_type/models/purchase_request.py
+++ purchase_request_type/models/purchase_request.py
@@ -3,19 +3,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
-from openerp.addons.purchase.purchase import purchase_order
 
 
 class PurchaseRequest(models.Model):
     _inherit = 'purchase.request'
 
+    STATES = {
+        'to_approve': [('readonly', True)],
+        'approved': [('readonly', True)],
+        'rejected': [('readonly', True)]
+    }
+
+    @api.model
     def _default_order_type(self):
         return self.env['purchase.order.type'].search([], limit=1)
 
-    order_type = fields.Many2one(comodel_name='purchase.order.type',
-                                 readonly=False,
-                                 states=purchase_order.READONLY_STATES,
-                                 string='Type',
-                                 ondelete='restrict',
-                                 default=_default_order_type)
-
+    order_type = fields.Many2one(
+        comodel_name='purchase.order.type',
+        states=STATES,
+        string='Type',
+        ondelete='restrict',
+        default=_default_order_type
+    )
